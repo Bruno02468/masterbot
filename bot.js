237bc6655554556c5307924e2dde995ca91f9694
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2014-10-19 17:08:02 -0200

    Added commands and fixed stuff.
    Added commands, fixed issues with logging and made the code readable.

--- bot.js
+++ bot.js
@@ -1,59 +1,114 @@
-/*Echobot v1.0. Designed by Get52. Nobody really cares who helped kel.
-Notes: use "disabled = true;" to turn off the bot 
-Database server, and postAndGet function and stuff to make it work better
-were all made by Bruno02468, including the github repo.*/
- 
-//setting variables
+/* Masterbot by get52 and completely revamped by Bruno02468.
+ * Run it using the run script on the GitHub repo to avoid need for updates.
+ */
+
+//Configuration variables
+
+var threshold = 50; //Time in seconds between sending messages (default: 50)
+var countdown = 60; //Time in seconds before the bot starts sending messages (default: 60)
+var nick      = "Masterbot" //Bot's nickname (default: Masterbot)
+var masters   = ["Bruno02468", "get52", "sammich", "Randomguy"]; //Bot's main controllers, they are the only ones who can toggle the bot
+
+//End of configuration variables
+
+
+//Adding the Array.contains utility function
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Defining some variables
 
 var logging = false;
 var disabled = false;
 var messages = [];
 
+//Booting up
+
 if (!logging) {
-  CLIENT.submit("/nick Masterbot");
-  CLIENT.submit("Masterbot by get52 and Bruno02468 now running.");
-  loggin = true;
+    CLIENT.submit("/nick " + nick);
+    CLIENT.submit("Masterbot by get52 and Bruno02468 now running.");
+    loggin = true;
 }
  
-//instantiating countdown and submit function
+//Start countdown
  
-setTimeout(function(){logging = false; submit();}, 60000);
+setTimeout(function(){logging = false; submit();}, countdown * 1000);
  
-//begin loggin' process
+//Begin logging process and listen for commands
 
 CLIENT.on('message', function(data){
-  if (logging) {
-     var text;
-     if (data.message[0] !== "$") {
+    var text = "";
+    var nick = data.nick;
+    
+    if (data.message[0] !== "$") {
         text = data.message.slice(1);
-     } else {
+    } else {
         text = data.message;
-     }
-     postAndGet(text);
-     console.log("logged: <" + text + ">");
-  }
+    }
+    
+    if (text == "!toggle") {
+        if (masters.contains(nick)) {
+            disabled = !disabled;
+            logging = !logging
+            if (disabled) {
+                CLIENT.submit("Masterbot has been disabled.");
+            } else {
+                CLIENT.submit("Masterbot has been enabled.");
+            }
+        } else {
+            CLIENT.submit("You do not have permission to do this, " + nick + ".");
+        }
+    } else if (text == "!masterbot") {
+        if (!disabled) {
+            var sendtext = messages[Math.floor(Math.random() * messages.length)];
+            CLIENT.submit(sendtext);
+        } else {
+            CILENT.submit("I'm currently disabled, try again later.");
+        }
+    } else if (text == "!masters") {
+        var msg = "My masters are: ";
+        for (var i = 0; i < masters.length - 2; i++) {
+            msg += masters[i] + ", ";
+        }
+        msg += "and " + masters[masters.length - 1] + ".";
+        CLIENT.submit(msg);
+    }
+    if (logging) {
+        postAndGet(text);
+        console.log("logged: <" + text + ">");
+    }
 });
+
+//Function that saves the message externally and updates the array
 
 function postAndGet(message) {
     if (message !== "Masterbot by get52 and Bruno02468 now running.") {
-      var xmlHttp = null;
-      xmlHttp = new XMLHttpRequest();
-      xmlHttp.open("GET", "http://bruno02468.com/spooks_bot/push.php?password=kekweed&message=" + encodeURIComponent(message) , false);
-      xmlHttp.send(null);
-      eval(xmlHttp.responseText.replace(/<br>/g, ""));
-      messages.pop();
+        var xmlHttp = null;
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", "http://bruno02468.com/spooks_bot/push.php?password=kekweed&message=" + encodeURIComponent(message) , false);
+        xmlHttp.send(null);
+        eval(xmlHttp.responseText.replace(/<br>/g, ""));
+        messages.pop();
     }
 }
  
 //IT BEGINS
  
 function submit() {
-   setInterval(function(){ submitAgain();}, 15000);
+    setInterval(function(){ submitAgain();}, threshold * 1000);
 }
  
 function submitAgain() {
-  if (!disabled) {
-    var sendtext = messages[Math.floor(Math.random() * messages.length)];
-    CLIENT.submit(sendtext);
-  }
+    if (!disabled) {
+        var sendtext = messages[Math.floor(Math.random() * messages.length)];
+        CLIENT.submit(sendtext);
+    }
 }
