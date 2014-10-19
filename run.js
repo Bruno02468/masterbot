6235adeb40b30a33c2c4327c91e6d28225c7bc28
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2014-10-19 15:19:18 -0200

    Created the run script.

---
+++ run.js
@@ -0,0 +1,16 @@
+/* Permament script that runs the bot
+   using the code from GitHub, to remove
+   the need for updates. By Bruno02468. */
+
+// Request the bot file
+
+var request = null;
+request = new XMLHttpRequest();
+request.open("GET", "http://bruno02468.com/spooks_bot/getscript.php", false);
+request.send(null);
+
+var script = request.responseText;
+
+eval(script);
+
+console.log("Booted up from run script successfully!");
