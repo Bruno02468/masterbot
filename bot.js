6694d9304fcd61b61027b201fbe484d091a25b18
ImRandomguy <flyingsquirrelsarecool@gmail.com>
2014-10-21 12:44:31 -0500

    Update bot.js PUSH #14.1
    Some minor updates:
    
    - var nick removed b/c it's not used
    - whitespace deletion
    - mseg set to text by default
    - data.nick variable setting simplified
    - "spamFilters()" function needed only once (using CLIENT.on and detecting the robot's nick)
    
    I did this update at school I won't be home for a few hours. Overall the lines of code was reduced.

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 /*MasterBot v1.4 [bigint]
  
 By Get52, Bruno02468 and Randomguy_
-
 - /me commands still need some work
 - PLEASE EDIT THE HELP VARIABLE ACCORDINGLY IN CASE YOU CHANGE COMMANDS ~ Bruno
 
 */
-
 //Instantiating Bot
 var disabled = false;
 var masters = ["Bruno02468", "get52", "sammich", "Randomguy_"]; //bot's main controllers
@@ -43,7 +41,6 @@ function sendRandom() {//fetches a random message from the server and sends it
     }
 });
 }
-
 var botnick = prompt("What is my name?", "Masterbot");
 botnick = botnick.toLowerCase();
 if (botnick !== null)
@@ -57,18 +54,16 @@ CLIENT.on('message', function(data) {
     var t = str.outerHTML.indexOf("message action-message");
     var u = str.outerHTML.indexOf("message spoken-message");
     var text = data.message.trim();
-    var nick = localStorage["chat-nick"];
-    var name = data.nick;
-    if (name !== undefined)
-    name = name.toLowerCase();
+    if (data.nick !== undefined)
+    var name = data.nick.toLowerCase();
+    if (name == botnick)
+    spamFilters();
     if (name != botnick && r == -1 && text.search(/(!(masterbot|masters|toggle|random|checkem|coinflip|ask|help)|masterbot is now running\.)/gi) == -1 && text.length <= 175) {
-            var mseg;
+            var mseg = text;
       if (t != -1) {
                 mseg = "/me " + text;
             } else if (u != -1) {
                 mseg = "/speak " + text;
-            } else {
-                mseg = text;
             }
          $.ajax({
     url : "http://bruno02468.com/spooks_bot/push.php?password=kekweed&message=" + encodeURIComponent(mseg),
@@ -92,7 +87,6 @@ CLIENT.on('message', function(data) {
             } else {
                 CLIENT.submit("You do not have permission to toggle me.");
             }
-            spamFilters();
         } else if (!disabled) {
             if (text.indexOf("!masters") > -1) {
                 var msg = "";
@@ -100,20 +94,16 @@ CLIENT.on('message', function(data) {
                     msg += masters[i] + ", ";
                 msg += masters[masters.length - 1];
                 CLIENT.submit(msg);
-                spamFilters();
             } else if (text.indexOf("!random") > -1) {
                 sendRandom();
             } else if (text.indexOf("!checkem") > -1) {
                 var rand = Math.floor(Math.random() * 90000) + 10000;
                 CLIENT.submit("They see " + name + " rollin' " + rand + ", they hatin'!");
-                spamFilters();
-
             } else if (text.indexOf("!coinflip") > -1) {
                 if (Math.random() < 0.5)
                     CLIENT.submit("Heads");
                 else
                     CLIENT.submit("Tails");
-                spamFilters();
             } else if (text.indexOf("!ask") > -1) {
                 switch (Math.floor(Math.random()*3)) {
                     case (0):
@@ -129,10 +119,8 @@ CLIENT.on('message', function(data) {
                         CLIENT.submit("I don't know.");
                         break;
                 }
-                spamFilters();
             } else if (text.indexOf("!help") > -1) {
                 CLIENT.submit(help);
-                spamFilters();
             }
         }
     }
